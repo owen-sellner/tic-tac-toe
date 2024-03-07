8365f7aaa9726f8b06542ed9a67f9dfbb12b195f
Owen Sellner <owen.sellner@gmail.com>
2024-03-07 12:26:26 -0500

    Install packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const test = false;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className={`text-[min(90vw,40vh)] uppercase font-bold ${test ? "text-red-600" : "text-blue-600"}`}>X O</h1>
+      <h1 className={`text-[min(50vw,10vh)] uppercase font-bold ${test ? "text-red-600" : "text-blue-600"}`}>winner!</h1>
+      {/* X SVG */}
+      <svg width="125" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <path d={"M 10,10 L 90,90"} stroke={"black"} stroke-width="20" />
+        <path d={"M 90,10 L 10,90"} stroke={"black"} stroke-width="20" />
+      </svg>
+      {/* CIRCLE SVG */}
+      <svg width="125" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle r="37" cx="50" cy="50" fill="none" stroke={"black"} stroke-width="20" />
+      </svg>
     </>
   )
 }
