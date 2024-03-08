6ea22325bf585285541919feababc1596cea5476
Owen Sellner <owen.sellner@gmail.com>
2024-03-08 13:16:06 -0500

    Add function to get for win/loss/draw

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,73 @@ import { useEffect, useState } from "react"
 function App() {
   const [isCircleTurn, setIsCircleTurn] = useState(false);
   const [gameGrid, setGameGrid] = useState(Array(9).fill(0));
+  const [winner, setWinner] = useState(-1);
+  const [winLocation, setWinLocation] = useState("");
+
+  const checkGrid = () => {
+    // COLUMNS
+    if (gameGrid[0] != 0 && gameGrid[0] == gameGrid[3] && gameGrid[3] == gameGrid[6]) {
+      setWinner(gameGrid[0])
+      // TODO: Add win location
+      return;
+    }
+    if (gameGrid[1] != 0 && gameGrid[1] == gameGrid[4] && gameGrid[4] == gameGrid[7]) {
+      setWinner(gameGrid[1])
+      // TODO: Add win location
+      return;
+    }
+    if (gameGrid[2] != 0 && gameGrid[2] == gameGrid[5] && gameGrid[5] == gameGrid[8]) {
+      setWinner(gameGrid[2])
+      // TODO: Add win location
+      return;
+    }
+    // ROWS
+    if (gameGrid[0] != 0 && gameGrid[0] == gameGrid[1] && gameGrid[1] == gameGrid[2]) {
+      setWinner(gameGrid[0])
+      // TODO: Add win location
+      return;
+    }
+    if (gameGrid[3] != 0 && gameGrid[3] == gameGrid[4] && gameGrid[4] == gameGrid[5]) {
+      setWinner(gameGrid[3])
+      // TODO: Add win location
+      return;
+    }
+    if (gameGrid[6] != 0 && gameGrid[6] == gameGrid[7] && gameGrid[7] == gameGrid[8]) {
+      setWinner(gameGrid[6])
+      // TODO: Add win location
+      return;
+    }
+    // DIAGONAL
+    if (gameGrid[0] != 0 && gameGrid[0] == gameGrid[4] && gameGrid[4] == gameGrid[8]) {
+      setWinner(gameGrid[0])
+      // TODO: Add win location
+      return;
+    }
+    if (gameGrid[2] != 0 && gameGrid[3] == gameGrid[4] && gameGrid[4] == gameGrid[6]) {
+      setWinner(gameGrid[2])
+      // TODO: Add win location
+      return;
+    }
+    // DRAW 
+    let result = true;
+    for (let i = 0; i < gameGrid.length; i++) {
+      if (gameGrid[i] == 0) {
+        result = false;
+      }
+    }
+    if (result) {
+      setWinner(0);
+    }
+  }
 
   useEffect(() => {
     setIsCircleTurn(!isCircleTurn);
+    checkGrid();
   }, [gameGrid, setGameGrid])
 
   return (
     <div className="flex flex-col min-h-screen items-center">
-      <Board gameGrid={gameGrid} setGameGrid={setGameGrid} isCircleTurn={isCircleTurn} />
+      <Board gameGrid={gameGrid} setGameGrid={setGameGrid} isCircleTurn={isCircleTurn} winner={winner} />
     </div>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,7 +3,7 @@ import GridLines from "./GridLines";
 import Toggle from "./Toggle";
 import CircleSvg from "./svg/CircleSvg";
 
-function Board({ gameGrid, setGameGrid, isCircleTurn }) {
+function Board({ gameGrid, setGameGrid, isCircleTurn, winner }) {
 
     const handleGridChange = (index) => {
         let newGameGrid = [...gameGrid];
@@ -21,7 +21,7 @@ function Board({ gameGrid, setGameGrid, isCircleTurn }) {
             <div className="relative grid grid-cols-3 grid-rows-3 gap-10">
                 {gameGrid.map((_, index) => (
                     <div key={index} className="flex items-center justify-center max-h-[125px] h-[15vw] max-w-[125px] w-[15vw] bg-keppel">
-                        {gameGrid[index] == 0 && <div onClick={() => { handleGridChange(index) }} className="h-[100%] w-[100%] z-10 cursor-pointer"></div>}
+                        {winner == -1 && gameGrid[index] == 0 && <div onClick={() => { handleGridChange(index) }} className="h-[100%] w-[100%] z-10 cursor-pointer"></div>}
                         {gameGrid[index] == 1 && <XSvg size="100%" drawn />}
                         {gameGrid[index] == 2 && <CircleSvg size="100%" drawn />}
                     </div>
