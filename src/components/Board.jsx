529fb3fa4badc0cf37f60049caa2dcef04f2e174
Owen Sellner <owen.sellner@gmail.com>
2024-03-08 15:27:08 -0500

    Fix grid on phone view

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -20,7 +20,7 @@ function Board({ gameGrid, setGameGrid, isCircleTurn, winner, winLocation }) {
         <>
             <Toggle isCircleTurn={isCircleTurn} />
             <div className="relative p-20">
-                <div className="relative grid grid-cols-3 grid-rows-3 gap-10">
+                <div className="relative grid grid-cols-3 grid-rows-3 gap-[1vw]">
                     {gameGrid.map((_, index) => (
                         <div key={index} className="flex items-center justify-center max-h-[125px] h-[15vw] max-w-[125px] w-[15vw] bg-keppel">
                             {winner === -1 && gameGrid[index] === 0 && <div onClick={() => { handleGridChange(index) }} className="h-[100%] w-[100%] z-10 cursor-pointer"></div>}
