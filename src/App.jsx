cc2503253241d9d13860586862a1f41b8f6e6750
Owen Sellner <owen.sellner@gmail.com>
2024-03-08 10:05:07 -0500

    Add X animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import XSvg from "./components/svg/Xsvg"
 import Board from "./components/Board"
 
 function App() {
@@ -8,13 +9,14 @@ function App() {
       </div>
       {/* X SVG */}
       <svg width="125" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <path d={"M 10,10 L 90,90"} stroke={"#393E41"} stroke-width="20" />
-        <path d={"M 90,10 L 10,90"} stroke={"#393E41"} stroke-width="20" />
+        <path d={"M 10,10 L 90,90"} stroke={"#393E41"} strokeWidth="20" />
+        <path d={"M 90,10 L 10,90"} stroke={"#393E41"} strokeWidth="20" />
       </svg>
       {/* CIRCLE SVG */}
       <svg width="125" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle r="37" cx="50" cy="50" fill="none" stroke={"#FFEED6"} stroke-width="20" />
+        <circle r="37" cx="50" cy="50" fill="none" stroke={"#FFEED6"} strokeWidth="20" />
       </svg>
+      <XSvg size="10vw" drawn />
     </>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,3 +1,4 @@
+import XSvg from "./svg/Xsvg";
 import GridLines from "./GridLines";
 
 function Board() {
@@ -7,10 +8,7 @@ function Board() {
             <div className="relative grid grid-cols-3 grid-rows-3 w-[max(25vw, 10vh)] h-[max(25vw, 10vh)] gap-10">
                 {Array(9).fill(0).map((_, index) => index).map((index) => (
                     <div key={index} className="flex items-center justify-center hover:bg-puce bg-keppel cursor-pointer">
-                        <svg width="125" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                            <path d={"M 10,10 L 90,90"} stroke={"#393E41"} stroke-width="20" />
-                            <path d={"M 90,10 L 10,90"} stroke={"#393E41"} stroke-width="20" />
-                        </svg>
+                        <XSvg size="7vw" drawn />
                     </div>
                 ))}
                 <GridLines />
